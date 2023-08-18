78c23aa6daaaf16d8d23f367d20a95f227a97aea
c-dez <hdezcruzcarlos@gmail.com>
2023-08-18 14:25:49 -0600

    added basic layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-export {content}
 
-import {menu} from '../src/modules/menu.js'
-
-// import {background} from '../src/modules/layout.js'
-import { makeContentElement } from './modules/content.js'
 
 const content = document.querySelector('#content')
-makeContentElement()
+content.style.backgroundColor = 'wheat'
+content.style.height = '100%'
+content.style.display = 'grid'
+content.style.gridTemplateRows = '50px repeat(5, 1fr)'
+content.style.gridTemplateColumns = 'repeat(1, 1fr)'
 
-content.appendChild(menu())
 
