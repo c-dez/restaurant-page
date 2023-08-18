ed0dca5af9dc5b3dc92117d2270c4698a8360ea0
c-dez <hdezcruzcarlos@gmail.com>
2023-08-18 14:31:07 -0600

    added item1

--- src/index.js
+++ src/index.js
@@ -7,4 +7,14 @@ content.style.display = 'grid'
 content.style.gridTemplateRows = '50px repeat(5, 1fr)'
 content.style.gridTemplateColumns = 'repeat(1, 1fr)'
 
+///title
+const title = document.createElement('div')
+title.textContent = 'MENU'
+content.appendChild(title)
+title.style.gridArea = '1/1/2/2'
 
+//item1
+const item1 = document.createElement('div')
+item1.textContent = 'taco: precio - icono'
+item1.style.gridArea = '2/1/3/2'
+content.appendChild(item1)
